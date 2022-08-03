1f704314a0e195fd9f8b320df33906adf755ba81
Arafat <arafatamrullaev021@gmail.com>
2022-08-03 11:29:52 +0600

    django_shop

---
+++ shop/account/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from .views import RegisterAPIView
+
+from rest_framework_simplejwt.views import  TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('register/', RegisterAPIView.as_view()),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+]
